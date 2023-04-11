c3f03d63e5a64ec6e1c0dae2cdfa7d24ba203700
Steven Sosebee <swsosebee@gmail.com>
2023-04-11 12:20:12 -0400

    hidden credentials in .ini file

--- php/config/config.php
+++ php/config/config.php
@@ -2,6 +2,15 @@
 
     define("PROJECT_ROOT_PATH", __DIR__ . "/../../");
     define("MODELS_PATH", __DIR__."/../models/");
+    $config = parse_ini_file(__DIR__.'/../../private/config.ini');
+    define("DB",$config['db']);
+    define("DB_HOST",$config['db_host']);
+    define("DB_USER",$config['db_userName']);
+    define("DB_PASSWORD",$config['db_password']);
+    define("DB_PORT",$config['port']);
+    
+    require MODELS_PATH."utility.php";
+    require MODELS_PATH."connection.php";
     require MODELS_PATH."baseModel.php";
     require MODELS_PATH."baseModel2.php";
     require MODELS_PATH."recipes.php";
@@ -9,10 +18,14 @@
     require MODELS_PATH."ingredients.php";
     require MODELS_PATH."steps.php";
     require MODELS_PATH."groceries.php";
+    require MODELS_PATH."groceries2.php";
     require MODELS_PATH."shoppingListItems.php";
     require MODELS_PATH."shoppingLists.php";
-    $params= json_decode(file_get_contents("php://input"),true);
     
+    $params= json_decode(file_get_contents("php://input"),true);
+    $model = $_SERVER['HTTP_CLASS'];
+    $action = $_SERVER['HTTP_ACTION'];
+    // $params['config'] = $config;
 // include the base controller file
 // require_once PROJECT_ROOT_PATH . "/php/Controller/Api/BaseController.php";
  
--- php/index.php
+++ php/index.php
@@ -1,19 +1,39 @@
 <?php
     require __DIR__ . "/config/config.php";
-    // require MODELS_PATH."baseModel.php";
-    // require MODELS_PATH."recipes.php";
-    // require MODELS_PATH."ingredients.php";
-    // require MODELS_PATH."recipes.php";
-
-    $model["Recipe"]= new Recipe2;
-    $model['Ingredient'] = new Ingredient;
-    $model["Test"] = new Base;
-    $model["Step"] = new Step;
-    $model["Grocery"] = new Grocery;
-    $model["ShoppingListItem"] = new ShoppingListItems;
-    $model['ShoppingList'] = new ShoppingList;
-
     
-    $model[$_SERVER['HTTP_CLASS']]->{$_SERVER['HTTP_ACTION']}($params);
+    if ($_SERVER['REQUEST_URI']=="/php/"){
+        $db = new DBConnection();
+        define("CONN",$db);
+    } else {
+        echo json_encode("not a valid request");
+        die();
+    }
+
+    // (new Base2())->testing($_SERVER['HTTP_CLASS'], $_SERVER['HTTP_ACTION'],$params);
+    try{
+    $res = (new $model($params))->{$action}();
+    $db->disconnect();
+    echo json_encode($res);
+    } catch(Exception $err){
+        $db->disconnect();
+        echo json_encode($err);
+    }
+    
+    
+    
+    
+        // echo json_encode($db);
+        // $res = $db->g;
+        // $res = $db->PDOConn->query("SELECT * FROM tblGroceries")->fetchAll();
+    
+    // $model["Recipe"]= new Recipe2;
+    // $model['Ingredient'] = new Ingredient;
+    // $model["Test"] = new Base;
+    // $model["Step"] = new Step;
+    // $model["Grocery"] = new Grocery;
+    // $model["ShoppingListItem"] = new ShoppingListItems;
+    // $model['ShoppingList'] = new ShoppingList;
+    
+    // $model[$_SERVER['HTTP_CLASS']]->{$_SERVER['HTTP_ACTION']}($params);
 
 ?>
